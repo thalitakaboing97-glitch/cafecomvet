41e83f0f0d42a086edcdc8aa36a0fe34636e4e69
thalitakaboing97-glitch <thalitakaboing97@gmail.com>
2025-11-26 17:52:33 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orgaYellow: "#FFCC00",
+        orgaOrange: "#F9A825",
+        orgaBlack: "#000000",
+        orgaGray: "#F7F7F7"
+      }
+    },
+  },
+  plugins: [],
+}
