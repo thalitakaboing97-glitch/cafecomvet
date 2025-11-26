25c0a3e17f524c8d3779347936df70dfc7061b39
thalitakaboing97-glitch <thalitakaboing97@gmail.com>
2025-11-26 18:36:08 -0300

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="mt-8 text-center py-4 text-gray-500 text-sm">
+      <p>Atualização em tempo real</p>
+    </footer>
+  );
+}
+
+export default Footer;
